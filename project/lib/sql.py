49da8488d342e6d9aaff9c8cc16d3073c95140dc
flex2016 <reloded2004@hotmail.com>
2018-04-03 20:27:02 -0400

    added lib to save models and updated views

---
+++ project/lib/sql.py
@@ -0,0 +1,23 @@
+from project import db
+
+class ResourceMixin(object):
+
+    def save(self):
+        """
+        Save a model instance.
+
+        :return: Model instance
+        """
+        db.session.add(self)
+        db.session.commit()
+
+        return self
+
+    def delete(self):
+        """
+        Delete a model instance.
+
+        :return: db.session.commit()'s result
+        """
+        db.session.delete(self)
+        return db.session.commit()
