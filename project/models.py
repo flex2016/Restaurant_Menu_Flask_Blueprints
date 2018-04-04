54980403921be1c5647746bfd366d224dfd7fcdb
flex2016 <reloded2004@hotmail.com>
2018-04-04 17:05:20 -0400

    updated models to work with heroku import issue

--- project/models.py
+++ project/models.py
@@ -1,9 +1,30 @@
 from project import db
-from lib.sql import ResourceMixin
 from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
 
 
+class ResourceMixin(object):
+
+    def save(self):
+        """
+        Save a model instance.
+
+        :return: Model instance
+        """
+        db.session.add(self)
+        db.session.commit()
+
+        return self
+
+
+    def delete(self):
+        """
+        Delete a model instance.
+
+        :return: db.session.commit()'s result
+        """
+        db.session.delete(self)
+        return db.session.commit()
 
 
 class User(db.Model):
