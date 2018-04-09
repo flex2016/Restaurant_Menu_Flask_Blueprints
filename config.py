182dcb0ecdba492a1783107218dabcaeb47e4320
flex2016 <reloded2004@hotmail.com>
2018-04-09 11:39:59 -0400

    updated config file

--- config.py
+++ config.py
@@ -6,14 +6,16 @@ class BaseConfig(object):
     DEBUG = False
     SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    SQLALCHEMY_TRACK_MODIFICATIONS= False # set to true to work with whoosh
+    SQLALCHEMY_TRACK_MODIFICATIONS = False   # set to true to work with whoosh
     # print SQLALCHEMY_DATABASE_URI
+
 
 class TestConfig(BaseConfig):
     DEBUG = True
     TESTING = True
     WTF_CSRF_ENABLED = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
